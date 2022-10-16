5116dad06f0a465d1e9aa7be74dc5dfc063df38f
Stevemech <stevemech2020@gmail.com>
2022-10-15 17:07:37 -0700

    yes

--- src/Test/Test-mission-pad.py
+++ src/Test/Test-mission-pad.py
@@ -5,6 +5,15 @@
 """
 
 from djitellopy import Tello
+import cv2
+
+
+# camera feed
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(1)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
 
 # create and connect
 tello = Tello()
@@ -12,8 +21,7 @@ tello.connect()
 
 # configure drone
 tello.enable_mission_pads()
-tello.set_mission_pad_detection_direction(1)  # forward detection only  只识别前方
-
+tello.set_mission_pad_detection_direction(1)
 tello.takeoff()
 
 pad = tello.get_mission_pad_id()
@@ -21,11 +29,9 @@ pad = tello.get_mission_pad_id()
 # detect and react to pads until we see pad #1
 while pad != 1:
     if pad == 3:
-        tello.move_back(30)
         tello.rotate_clockwise(90)
 
     if pad == 4:
-        tello.move_up(30)
         tello.flip_forward()
 
     pad = tello.get_mission_pad_id()
