ef5a5adcd0adb49735436535767f6df64b5dd6a9
jypan2007 <jypan2007@gmail.com>
2022-10-28 15:58:08 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,71 +6,75 @@
 """
 from time import sleep
 from djitellopy import tello
+from modules import keyPressModule as kpm
+from time import sleep
 from getBattery import baReminder
 import cv2 as cv
 from cv2 import aruco
-import pygame
-
-def init():
-    pygame.init()
-    win = pygame.display.set_mode((400,400))
-
-def getKey(keyName):
-    ans = False
-    for eve in pygame.event.get(): pass
-    keyInput = pygame.key.get_pressed()
-    myKey = getattr(pygame, 'K_{}'.format(keyName))
-    if keyInput[myKey]:
-        ans = True
-    pygame.display.update()
-    return ans
-
-def main():
-    init()
-    drone = tello.Tello()
-    drone.connect()
-    print(drone.get_battery())
-
-    drone.takeoff()
-    if getKey("UP"):
-        print("UP key pressed")
-        drone.move_up(60)
-#    sleep(60)
-
-    drone.streamoff()
-    drone.streamon()
-    marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-
-    param_markers = aruco.DetectorParameters_create()
-
- #   cam = cv.VideoCapture(1)
+import numpy as np
 
 
+kpm.init()
+drone = tello.Tello()
+drone.connect()
+baReminder()
+drone.streamoff()
+drone.streamon()
+marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+param_markers = aruco.DetectorParameters_create()
 
-    while True:
-        frame = drone.get_frame_read().frame
+#cap = cv.VideoCapture(0)
 
-        frame = cv.resize(frame, (360, 240))
-        cv.imshow("frame", frame)
+def getKeyBoardInput():
+    # lr is left & right
+    # fb is front & back
+    # ud is up & down
+    # yv is yaw & velocity
+    lr, fb, ud,  yv = 0, 0, 0, 0
+    speed = 50
 
-        grey_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        marker_corners, marker_IDs, reject = aruco.detectMarkers(
-            grey_frame, marker_dict, parameters=param_markers
-        )
+    if kpm.getKey("LEFT"): lr  = -speed
+    elif kpm.getKey("RIGHT"): lr  = speed
+
+    if kpm.getKey("UP"): fb  = speed
+
+    elif kpm.getKey("DOWN"): fb  = -speed
+
+    if kpm.getKey("w"): ud  = speed
+    elif kpm.getKey("s"): ud  = -speed
+
+    if kpm.getKey("a"): yv  = speed
+    elif kpm.getKey("d"): yv  = -speed
+
+    if kpm.getKey("q"): drone.land()
+    if kpm.getKey("t"): drone.takeoff()
+    return [lr, fb, ud, yv]
+
+
+while True:
+    vals = getKeyBoardInput()
+    drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+
+#    frame = cap.read()
+    frame = drone.get_frame_read().frame
+    frame = cv.resize(frame, (360, 240))
+
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    marker_corners, marker_IDs, reject = aruco.detectMarkers(
+        gray_frame, marker_dict, parameters=param_markers
+    )
+
+    if marker_corners:
         for ids, corners in zip(marker_IDs, marker_corners):
-            print(ids, " ", corners)
+           cv.polylines(
+               frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
+           )
+           #print(ids, " ", corners)
+    cv.imshow("frame", frame)
+    cv.waitKey(1)
+
+#cap.release()
+cv.destroyAllWindows()
 
 
-        cv.waitKey(1)
-        if getKey("s"):
-            print("s key pressed")
-            break
-#    cam.release()
-    cv.destroyAllWindows()
 
-    drone.land()
-
-    # Function checks the battery status and prints out the exact percentage
-    baReminder()
-
-main()
