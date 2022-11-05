702adeee38fc469f63902d92066fb817fd6867f0
jypan2007 <jypan2007@gmail.com>
2022-11-05 16:59:28 -0700

    yes

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ import math
 wid1 = 0
 wid2 = 15
 wid3 = 26
-
+targetList = [0, 15, 26]
 
 cidlist = []
 cidxlist = []
@@ -44,8 +44,8 @@ camera_matrix = np.array([[2.13243514e+03, 0.00000000e+00, 1.33300826e+03],
 distortion_coefficients = np.array([[0.20388981, -0.66295284, -0.0584427, 0.00772092, 1.17854164]])
 
 axis = np.array([[marker_length, 0, 0], [0, marker_length, 0], [0, 0, marker_length * -1]]).reshape(-1, 3)
-#cap = cv.VideoCapture(0)
 
+marker_range = [6200, 6800]
 def getKeyBoardInput():
     # lr is left & right
     # fb is front & back
@@ -71,101 +71,107 @@ def getKeyBoardInput():
     if kpm.getKey("t"): drone.takeoff()
     return [lr, fb, ud, yv]
 
+
+def trackARuCO(tvec1, area):
+    fb =0
+    x = tvec1[0][0][0]
+    y = tvec1[0][0][1]
+    z = tvec1[0][0][2]
+
+#    if area > marker_range[0] and area < marker_range[1]:
+#        fb = 0
+#    elif area > marker_range[1]:
+#        fb = -20
+#    elif area < marker_range[0] and area !=0:
+#        fb = 20
+
+
+    if z < 200:
+        fb = -20
+    elif z > 200:
+        fb = 20
+    else:
+        fb = 0
+
+    if x > 100:
+        lr = 20
+    elif x < -100:
+        lr = -20
+    else:
+        lr = 0
+
+    if y > 100:
+        ud = 20
+    elif y < -100:
+        ud = -20
+    else:
+        ud = 0
+
+drone.takeoff()
+drone.move_up(50)
+
 while True:
     vals = getKeyBoardInput()
     drone.send_rc_control(vals[0], vals[1], vals[2], vals[3])
 
 #    frame = cap.read()
     frame = drone.get_frame_read().frame
-    frame = cv.resize(frame, (640, 480 ))
+    frame = cv.resize(frame, (360, 240))
 
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     marker_corners, marker_IDs, reject = aruco.detectMarkers(
         gray_frame, marker_dict, parameters=param_markers
     )
-    #aruco.drawDetectedMarkers(frame, marker_corners, marker_IDs)
     if marker_IDs is not None:
         img_aruco = aruco.drawDetectedMarkers(frame, marker_corners, marker_IDs, (0, 255, 0))
 
+        if len(marker_IDs) == 9:
+            for ids1, marker_corners1 in zip(marker_IDs, marker_corners):
+                print(ids1, " ", marker_corners1.astype(np.int32))
+                if ids1[0] == wid1 or ids1[0] == wid2 or ids1[0] == wid3:
+                    cidlist.append(ids1[0])
+                    cidxlist.append(marker_corners1[0])
+            print("cidlist:", cidlist)
+            print("cidxlist: ", cidxlist)
         # tvec is the center of the marker in the camera's world
-        rvec, tvec, _ = aruco.estimatePoseSingleMarkers(marker_corners, marker_length, camera_matrix,
+            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(cidxlist, marker_length, camera_matrix,
                                                         distortion_coefficients)
 
-        #print("tvec", tvec)
-        #print("rvec", rvec)
+            print("tvec", tvec)
+            print("rvec", rvec)
 
-        # In case there are multiple markers
-        for i in range(marker_IDs.size):
-            img_aruco = cv.drawFrameAxes(img_aruco, camera_matrix, distortion_coefficients, rvec[i], tvec[i],
+            # In case there are multiple markers
+            for i in range(len(cidlist)):
+                img_aruco = cv.drawFrameAxes(img_aruco, camera_matrix, distortion_coefficients, rvec[i], tvec[i],
                                          marker_length, 3)
-#           img_aruco = aruco.drawAxis(img_aruco, camera_matrix, distortion_coefficients, rvec[i], tvec[i],
-#                                       marker_length)
-
-#        if rvec.size == 3:
-#            imgpts, _ = cv.projectPoints(axis, rvec, tvec, camera_matrix, distortion_coefficients)
-
-#            p1 = (int(imgpts[0][0][0]), int(imgpts[0][0][1]))
-
-#            if p1 is not None:
-#                cv.circle(img_aruco, p1, 5, (0, 0, 255), -1)
-
-#            p2 = (int(imgpts[1][0][0]), int(imgpts[1][0][1]))
-
-#            if p2 is not None:
-#                cv.circle(img_aruco, p2, 5, (0, 255, 0), -1)
-
-#            p3 = (int(imgpts[2][0][0]), int(imgpts[2][0][1]))
-
-#            if p3 is not None:
-#                cv.circle(img_aruco, p3, 5, (255, 0, 0), -1)
-
-#            # Plot a point at the center
-#            cv.circle(img_aruco, (480, 320), 5, (0, 255, 0), -1)
 
             tvec_x = tvec[0][0][0]
             tvec_y = tvec[0][0][1]
-            tvec_z = tvec[0][0][2]
+            drone.move_forward((int(tvec[0][0][2]/10)))
+            if (int(tvec_x/16) > 20):
+                drone.move_right((int(tvec_x/16)))
+            elif (int(tvec_x/(-16) > 20)):
+                drone.move_left(int(tvec_x/(-16)))
+            if (int(tvec_y/16)) > 20:
+                drone.move_up(int(tvec_y/16))
+            elif (int(tvec_y/(-16)) > 20):
+                drone.move_down(int(tvec_y/(-16)))
+        elif len(marker_IDs) == 1:
+            print("only see one marker now")
+            for ids1, marker_corners1 in zip(marker_IDs, marker_corners):
+                print(ids1, " ", marker_corners1.astype(np.int32))
+                if ids1[0] in targetList:
+                    rvec1, tvec1, _ = aruco.estimatePoseSingleMarkers(marker_corners1, marker_length, camera_matrix,
+                                                                      distortion_coefficients)
+                    print("tvec1:", tvec1)
+                    print("ids1:", ids1)
+                    area = (marker_corners1[0][1]-marker_corners1[0][0])*(marker_corners1[0][2]-marker_corners1[0][1])
+                    trackARuCO(tvec1, area)
 
-            # Distance from camera is the magnitude of tvec
-            distance = math.sqrt(tvec_x * tvec_x + tvec_y * tvec_y + tvec_z * tvec_z)
 
-            # Let's focus on keeping the marker centered on the x axis (roll left/right)
-            # This means we'll consider y and z constant for this demonstration
-
-            # Calculate angle of vectors
-            array = np.array([tvec_x, tvec_y, tvec_z])
-            array_mag = np.linalg.norm(array)
-
-            # Vector to center of screen
-            array2 = np.array([0, 0, tvec_z])
-            array2_mag = np.linalg.norm(array2)
-
-            dot = np.dot(array, array2)
-
-            # Solve for angle
-            cos = np.arccos(dot / (array_mag * array2_mag))
-
-            degrees = np.degrees(cos)
-
-    #        for ids, corners in zip(marker_IDs, marker_corners):
-#            cv.polylines(
-#               frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
-#            )
-        if len(marker_IDs) == 18 and count == 0:
-            count = 1
-            list18 = zip(marker_IDs, marker_corners)
-            for ids1, marker_corners1 in list18:
-                # print(ids1, " ", corners1.astype(np.int32))
-                if ids1[0] == wid1 or ids1[0] == wid2 or ids1[0] == wid3:
-                    cidlist.append(ids1[0])
-                    cidxlist.append(marker_corners1[0][0][0])
-                    cidxlist.sort()
     cv.imshow("frame", frame)
     cv.waitKey(1)
-    print("***************one list printed****************")
 
-print(cidlist)
-print(cidxlist)
 
 #drone.land()
 cv.destroyAllWindows()
